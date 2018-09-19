71dd881158ad657c3b04047369e3102c411c1976
Rob Gaston <rgaston@fargeo.com>
2018-09-18 19:35:06 -0700

    updates for address datatype and gecoder widget

--- extensions/datatypes/address/address.py
+++ extensions/datatypes/address/address.py
@@ -22,15 +22,31 @@ class AddressDataType(BaseDataType):
 
     def validate(self, value, source=None):
         errors = []
+        try:
+            value['placeName']
+            value['x']
+            value['y']
+        except KeyError:
+            errors.append({
+                'type': 'ERROR',
+                'message': 'datatype: {0} value: {1} {2} - {3}. {4}'.format(
+                    self.datatype_model.datatype,
+                    value,
+                    source,
+                    'missing required properties',
+                    'This data was not imported.'
+                )
+            })
+
         return errors
 
     def append_to_document(self, document, nodevalue, nodeid, tile):
-        return
+        document['strings'].append({'string': nodevalue['placeName'], 'nodegroup_id': tile.nodegroup_id})
 
     def transform_export_values(self, value, *args, **kwargs):
         if value is not None:
             return value
 
     def get_search_terms(self, nodevalue, nodeid=None):
-        terms = []
+        terms = [nodevalue['placeName']]
         return terms
--- extensions/widgets/geocoder/geocoder.js
+++ extensions/widgets/geocoder/geocoder.js
@@ -1,62 +1,39 @@
 define([
     'knockout',
     'viewmodels/widget',
-    'arches'
+    'arches',
+    'bindings/select2-query'
 ], function (ko, WidgetViewModel, arches) {
     return ko.components.register('geocoder', {
         viewModel: function(params) {
             var self = this;
+            this.valueProperties = ['placeName','x','y']
             WidgetViewModel.apply(this, [params]);
-            
-            if (ko.isObservable(this.value)){
-                this.placeName = ko.observable();
-                this.x = ko.observable();
-                this.y = ko.observable();
-                this.placeName.subscribe(function() {
-                        this.value({
-                            placeName: this.placeName(),
-                            x: this.x(),
-                            y: this.y()
-                        });
-                }, this);
-            } else {
-                this.placeName = this.value.placeName;
-                this.x = this.value.x;
-                this.y = this.value.y;
-            }
                         
-            var getAddressData = function(term, callback) {
-                var url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
-                    term + '.json';
-                $.getJSON(url, {
-                    access_token: arches.mapboxApiKey,
-                    types: 'address',
-                    limit: 10
-                }, function (data) {
-                    callback(data);
-                });
-            };
-            
             this.select2Config = {
                 value: this.placeName,
-                clickBubble: true,
-                multiple: false,
-                placeholder: ko.observable('blah'),
-                allowClear: true,
                 query: function (query) {
                     if (query.term.length < 3) {
                         query.callback({results: []});
                         return;
                     }
-                    getAddressData(query.term, function (data) {
+                    var url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' +
+                        query.term + '.json';
+                    $.getJSON(url, {
+                        access_token: arches.mapboxApiKey,
+                        types: 'address',
+                        limit: 10
+                    }, function (data) {
                         query.callback({results: data.features});
                     });
                 },
                 initSelection: function(element, callback) {
-                    getAddressData($(element).val(), function (data) {
-                        if (data.features.length > 0) {
-                            callback(data.features[0]);
-                        }
+                    callback({
+                        place_name: self.placeName(),
+                        center: [
+                            self.x(),
+                            self.y()
+                        ]
                     });
                 },
                 id: function(selection) {
