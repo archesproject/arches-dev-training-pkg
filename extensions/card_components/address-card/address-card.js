d1f38122896054e122fddf0a43df21567b8ffc50
Rob Gaston <rgaston@fargeo.com>
2018-09-19 17:48:16 -0700

    adds address points to map

--- extensions/card_components/address-card/address-card.js
+++ extensions/card_components/address-card/address-card.js
@@ -1,14 +1,61 @@
 define([
     'knockout',
+    'underscore',
     'viewmodels/card-component',
     'bindings/mapbox-gl'
-], function(ko, CardComponentViewModel) {
+], function(ko, _, CardComponentViewModel) {
     return ko.components.register('address-card', {
         viewModel: function(params) {
+            var self = this;
             CardComponentViewModel.apply(this, [params]);
+            this.fc = ko.computed(function() {
+                var fc = {
+                    type: 'FeatureCollection',
+                    features: []
+                };
+                _.each(self.getValuesByDatatype('address'), function(data, id) {
+                    if (data.value) {
+                        fc.features.push({
+                            id: id,
+                            properties: {
+                                name: data.name,
+                                address: data.value.address
+                            },
+                            geometry: {
+                                "type": "Point",
+                                "coordinates": [
+                                    data.value.x,
+                                    data.value.y
+                                ]
+                            }
+                        })
+                    }
+                });
+                return fc;
+            });
             
             this.setupMap = function(map) {
-                console.log(map);
+                map.on('load', function () {
+                    map.addSource('address-points', {
+                        'type': 'geojson',
+                        'data': self.fc()
+                    });
+                    map.addLayer({
+                        'id': 'address-points',
+                        'source': 'address-points',
+                        'type': 'symbol',
+                        'layout': {
+                            'icon-image': 'star-15',
+                            'text-field': '{name}: {address}',
+                            'text-offset': [0, 0.6],
+                            'text-anchor': "top"
+                        }
+                    });
+                    
+                    self.fc.subscribe(function(fc) {
+                        map.getSource('address-points').setData(fc);
+                    });
+                });
             };
         },
         template: {
