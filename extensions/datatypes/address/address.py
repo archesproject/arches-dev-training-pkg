3f4e289062321d13921d62c4daacf48399e736e9
Rob Gaston <rgaston@fargeo.com>
2018-10-10 11:30:25 -0700

    updating address data type...

--- extensions/datatypes/address/address.py
+++ extensions/datatypes/address/address.py
@@ -1,15 +1,12 @@
 from arches.app.datatypes.base import BaseDataType
 from arches.app.models import models
-from arches.app.models.system_settings import settings
-
-geocoder = models.Widget.objects.get(name='geocoder')
 
 details = {
     'datatype': 'address',
     'iconclass': 'fa fa-file-code-o',
     'modulename': 'datatypes.py',
     'classname': 'AddressDataType',
-    'defaultwidget': geocoder,
+    'defaultwidget': None,
     'defaultconfig': None,
     'configcomponent': None,
     'configname': None,
@@ -27,22 +24,19 @@ class AddressDataType(BaseDataType):
             value['x']
             value['y']
         except KeyError:
+            message = 'datatype: address, value: %s - missing required properties. data was not imported.'
             errors.append({
                 'type': 'ERROR',
-                'message': 'datatype: {0} value: {1} {2} - {3}. {4}'.format(
-                    self.datatype_model.datatype,
-                    value,
-                    source,
-                    'missing required properties',
-                    'This data was not imported.'
-                )
+                'message': message.format(value)
             })
 
         return errors
 
     def append_to_document(self, document, nodevalue, nodeid, tile):
-        document['strings'].append({'string': nodevalue['address'], 'nodegroup_id': tile.nodegroup_id})
+        document['strings'].append({
+            'string': nodevalue['address'],
+            'nodegroup_id': tile.nodegroup_id
+        })
 
     def get_search_terms(self, nodevalue, nodeid=None):
-        terms = [nodevalue['address']]
-        return terms
+        return [nodevalue['address']]
